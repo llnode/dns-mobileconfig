430ae9d48baec151266d25e64972c4ef3846fc2e
Jakob <login@notjakob.com>
2021-11-12 17:47:00 +0100

    add editor for single configs

--- README.md
+++ README.md
@@ -15,6 +15,7 @@ For more information, see the website of the tool itself: https://dns.notjakob.c
 
 - Eli Grey for [FileSaver.js](https://github.com/eligrey/FileSaver.js)
 - uuidjs for the [UUID JavaScript library](https://github.com/uuidjs/uuid)
+- Nathan Rajlich for [plist.js](https://github.com/TooTallNate/plist.js)
 - Paul Miller for [his excellent article](https://paulmillr.com/posts/encrypted-dns/) and the [premade profiles](https://github.com/paulmillr/encrypted-dns)
 - W3schools for [w3.css](https://www.w3schools.com/w3css/)
 - nitrohorse for [encrypted-dns.party](https://encrypted-dns.party) and the profiles hosted there
--- js/tool.js
+++ js/tool.js
@@ -15,30 +15,35 @@ function getCookie(cname) {
 }
 
 function addToList() {
-    var runningNo = getCookie("runningNo");
-    if (getCookie("runningNo") == "") {
-        runningNo = 0;
+    if (document.getElementById("serverUrl").value.includes(":")) {
+        alert("Entering custom ports (e.g. :853) is not supported. Please remove it.");
     }
+    else {
+        var runningNo = getCookie("runningNo");
+        if (getCookie("runningNo") == "") {
+            runningNo = 0;
+        }
+        
+        var d = new Date();
+        d.setTime(d.getTime() + (86400000)); //expires in 24h
+        var expires = "expires="+ d.toUTCString();
+        document.cookie = runningNo + "provName=" + encodeURIComponent(document.getElementById("provName").value) + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "doh=" + document.getElementById("doh").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "dns1v4=" + document.getElementById("dns1v4").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "dns2v4=" + document.getElementById("dns2v4").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "dns1v6=" + document.getElementById("dns1v6").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "dns2v6=" + document.getElementById("dns2v6").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "serverUrl=" + document.getElementById("serverUrl").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "exclWifi=" + encodeURIComponent(document.getElementById("exclWifi").value) + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "useWifi=" + document.getElementById("useWifi").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "useCell=" + document.getElementById("useCell").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
+        document.cookie = runningNo + "lockProfile=" + document.getElementById("lockProfile").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
     
-    var d = new Date();
-    d.setTime(d.getTime() + (86400000)); //expires in 24h
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = runningNo + "provName=" + encodeURIComponent(document.getElementById("provName").value) + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "doh=" + document.getElementById("doh").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "dns1v4=" + document.getElementById("dns1v4").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "dns2v4=" + document.getElementById("dns2v4").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "dns1v6=" + document.getElementById("dns1v6").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "dns2v6=" + document.getElementById("dns2v6").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "serverUrl=" + document.getElementById("serverUrl").value + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "exclWifi=" + encodeURIComponent(document.getElementById("exclWifi").value) + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "useWifi=" + document.getElementById("useWifi").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "useCell=" + document.getElementById("useCell").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
-    document.cookie = runningNo + "lockProfile=" + document.getElementById("lockProfile").checked + ";" + expires + ";path=/; SameSite=Strict; Secure";
-
-    runningNo = runningNo + 1;
-    document.cookie = "runningNo=" + runningNo + ";" + expires + ";path=/; SameSite=Strict; Secure";
-
-    window.location.href = "finalize.html";
+        runningNo = runningNo + 1;
+        document.cookie = "runningNo=" + runningNo + ";" + expires + ";path=/; SameSite=Strict; Secure";
+    
+        window.location.href = "finalize.html";
+    }
 }
 
 function switchToHTTPS() {
@@ -77,6 +82,77 @@ function getDataFromUpload() {
 }
 
 function handleProfileText(uploadedProfile) {
-    //TODO
-    //Important: Handle missing info in file: alert and abort!
+    var profile = plist.parse(uploadedProfile);
+    console.log(profile);
+
+    //Name
+    document.getElementById("provName").value = profile.PayloadDisplayName;
+
+    //Protocol and URL/ServerName
+    if (profile.PayloadContent[0].DNSSettings.DNSProtocol == "HTTPS") {
+        document.getElementById("doh").checked = true;
+        document.getElementById("serverUrl").value = profile.PayloadContent[0].DNSSettings.ServerURL;
+    }
+    else if(profile.PayloadContent[0].DNSSettings.DNSProtocol == "TLS") {
+        document.getElementById("dot").checked = true;
+        document.getElementById("serverUrl").value = profile.PayloadContent[0].DNSSettings.ServerName;
+    }
+
+    //ServerAddresses, if applicable
+    const serverAddresses = profile.PayloadContent[0].DNSSettings.ServerAddresses;
+    if (serverAddresses) {
+        var dns4count = 0;
+        var dns6count = 0;
+        for (let index = 0; index < serverAddresses.length; index++) {
+            if (serverAddresses[index].includes(":")) {
+                if (dns6count == 0) {
+                    document.getElementById("dns1v6").value = serverAddresses[index];
+                }
+                else {
+                    document.getElementById("dns2v6").value = serverAddresses[index];
+                }
+                dns6count++;
+            }
+            else if (serverAddresses[index].includes(".")) {
+                if (dns4count == 0) {
+                    document.getElementById("dns1v4").value = serverAddresses[index];
+                }
+                else {
+                    document.getElementById("dns2v4").value = serverAddresses[index];
+                }
+                dns4count++;
+            }
+        }
+    }
+
+    //Profile locked?
+    document.getElementById("lockProfile").checked = profile.PayloadContent[0].ProhibitDisablement;
+
+    //Allow Wi-Fi and Cellular, check for excluded SSIDs
+    profile.PayloadContent[0].OnDemandRules.forEach(rule => {
+        if (rule.InterfaceTypeMatch == "WiFi") {
+            if (rule.Action == "Connect") {
+                document.getElementById("useWifi").checked = true;
+            }
+            else {
+                document.getElementById("useWifi").checked = false;
+            }
+        }
+        else if (rule.InterfaceTypeMatch == "Cellular") {
+            if (rule.Action == "Connect") {
+                document.getElementById("useCell").checked = true;
+            }
+            else {
+                document.getElementById("useCell").checked = false;
+            }
+        }
+        else if (rule.SSIDMatch) {
+            rule.SSIDMatch.forEach(ssid => {
+                if (document.getElementById("exclWifi").value != "") {
+                    document.getElementById("exclWifi").value += ", "
+                }
+                document.getElementById("exclWifi").value += ssid;
+            });
+        }
+    });
 }
